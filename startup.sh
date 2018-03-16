cb07973b9e5789e76722d5e37ee02207e591017e
kyle <aizuguo_@126.com>
2018-03-16 17:04:33 +0800

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,17 @@
+name=jinshou
+port=9999
+WORKSPACE=${WORKSPACE:=$PWD}
+
+if [ "$(sudo docker ps -aq -f name=$name)" ]; then
+	# stop and run
+	sudo docker stop $name && sudo docker rm $name && sudo docker run --name $name -d -p $port:80 -v $WORKSPACE/web:/usr/share/nginx/html nginx
+else
+	# run
+	sudo docker run --name $name -d -p $port:80 -v $WORKSPACE/web:/usr/share/nginx/html nginx
+fi
+
+
+
+
+
+
